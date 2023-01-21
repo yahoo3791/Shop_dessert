248934686b5246360d90cb3388aec9a77beba683
JIE <youco1327@yahoo.com.tw>
2023-01-21 14:54:40 +0800

    refactor: main.js

--- src/main.js
+++ src/main.js
@@ -1,22 +1,33 @@
 import { createApp } from 'vue';
+
 import VueLoading from 'vue-loading-overlay';
+
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+
 import 'bootstrap';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+
 import 'animate.css';
+
 import WaveUI from 'wave-ui';
 import 'wave-ui/dist/wave-ui.css';
+
 import 'vue-loading-overlay/dist/vue-loading.css';
+
 import VueEasyLightbox from 'vue-easy-lightbox';
+
 import {
   Field, Form, ErrorMessage, defineRule, configure,
 } from 'vee-validate';
 import { required, email, min } from '@vee-validate/rules';
 import { localize, setLocale } from '@vee-validate/i18n';
 import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json';
+
 import AOS from 'aos';
+
 import { date, currency } from './methods/filters';
+
 import App from './App.vue';
 import router from './router';
 import 'aos/dist/aos.css';
@@ -31,17 +42,22 @@ configure({
 setLocale('zh_TW');
 const app = createApp(App);
 (() => new WaveUI(app, {}))();
+
 app.component('VForm', Form);
 app.component('VField', Field);
 app.component('ErrorMessage', ErrorMessage);
+
 app.config.globalProperties.$filters = {
   currency, date,
 };
+
 app.use(VueLoading);
 app.use(VueAxios, axios);
 app.use(router);
 app.use(VueEasyLightbox);
+
 AOS.init({
   duration: 1200,
 });
+
 app.mount('#app');
